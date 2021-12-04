52693788871b82956489ab61b4a60f9ad0d60ce7
Gus Nordhielm <gus.nordhielm@gmail.com>
2021-12-04 12:54:49 -0500

    add basic example with test code

---
+++ examples/basic/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+
+const Example = () => <div>HI</div>;
+
+ReactDOM.render(<Example />, document.getElementById('root'));
