6676f5bb1dd8be19a4216769aa439aad6824c0e6
Gus Nordhielm <gus.nordhielm@gmail.com>
2021-12-04 13:48:12 -0500

    add resource dep, starter code

--- examples/basic/index.js
+++ examples/basic/index.js
@@ -1,7 +1,129 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { describeResource, describeMutation } from '@alphaflow/resource';
+import { getTodos, addTodo, removeTodo, setTodoIsChecked } from './services';
 import './style.css';
 
-const Example = () => <div>HI</div>;
+const TodoListResource = describeResource('TodoList', {
+  get: async () => (await getTodos()) || [],
+});
 
-ReactDOM.render(<Example />, document.getElementById('root'));
+const addTodoMutation = describeMutation('addTodo', async text => {
+  try {
+    const newTodo = await addTodo(text);
+    await TodoListResource.refresh();
+    return newTodo;
+  } catch (error) {
+    console.error('addTodo failed', error);
+  }
+});
+
+const removeTodoMutation = describeMutation('removeTodo', async todoId => {
+  try {
+    await removeTodo(todoId);
+    await TodoListResource.refresh();
+  } catch (error) {
+    console.error('removeTodo failed', error);
+  }
+});
+
+const setTodoIsCheckedMutation = describeMutation(
+  'setTodoIsChecked',
+  async ({ todoId, isChecked }) => {
+    await TodoListResource.yield(todoId, (last = {}) => ({
+      ...last,
+      isChecked,
+    }));
+
+    try {
+      await setTodoIsChecked({ todoId, isChecked });
+    } catch (error) {
+      console.error('setTodoIsChecked failed', error);
+    }
+  },
+);
+
+const TodoList = () => {
+  const [todoList, error] = TodoListResource.use();
+
+  if (error)
+    return (
+      <div>
+        <p>Sorry, we were unable to retrieve todos.</p>
+        <pre>{error.message}</pre>
+      </div>
+    );
+
+  if (todoList)
+    if (!todoList.length) return 'No todos.';
+    else
+      return (
+        <div>
+          {todoList.map(({ id, text, isChecked }) => (
+            <div key={id}>
+              <input
+                type="checkbox"
+                defaultChecked={isChecked}
+                onChange={event => {
+                  setTodoIsCheckedMutation({
+                    todoId: id,
+                    isChecked: event.target.checked,
+                  });
+                }}
+              />
+              {text}
+              <button
+                onClick={() => {
+                  removeTodoMutation(id);
+                }}
+              >
+                x
+              </button>
+            </div>
+          ))}
+        </div>
+      );
+
+  return 'Loading...';
+};
+
+const App = () => {
+  const [newInputText, setNewInputText] = useState('');
+
+  const [isAddingTodo, setIsAddingTodo] = useState(false);
+
+  const handleAddTodo = useCallback(async () => {
+    if (!newInputText) return;
+    setIsAddingTodo(true);
+    const newTodo = await addTodoMutation(newInputText);
+    setIsAddingTodo(false);
+
+    if (newTodo) setNewInputText('');
+  }, [newInputText, setIsAddingTodo]);
+
+  return (
+    <div>
+      <h1>Todo App</h1>
+      <p>
+        {isAddingTodo ? (
+          'Adding...'
+        ) : (
+          <>
+            <input
+              placeholder="Add a todo..."
+              type="text"
+              value={newInputText}
+              onChange={event => {
+                setNewInputText(event.target.value);
+              }}
+            />
+            <button onClick={handleAddTodo}>Add</button>
+          </>
+        )}
+      </p>
+      <TodoList />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ examples/basic/services.js
@@ -0,0 +1,48 @@
+const KEY = 'resource.examples.basic';
+
+const latency = () =>
+  new Promise(resolve => {
+    const timeout = Math.random() * (1000 - 100) + 100;
+    setTimeout(resolve, timeout);
+  });
+
+const read = async () => {
+  await latency();
+
+  const data = JSON.parse(localStorage.getItem(KEY) || '[]');
+
+  return data;
+};
+
+const write = async makeToWrite => {
+  const toWrite = JSON.stringify(makeToWrite(await read()));
+
+  localStorage.setItem(KEY, toWrite);
+};
+
+export const getTodos = async () => {
+  const data = (await read()) || [];
+  return data;
+};
+
+export const addTodo = async text => {
+  const newTodo = {
+    id: Date.now(),
+    text,
+    isChecked: false,
+  };
+
+  await write(last => [newTodo, ...last]);
+
+  return newTodo;
+};
+
+export const removeTodo = async todoId => {
+  await write(last => last.filter(item => item.id !== todoId));
+};
+
+export const setTodoIsChecked = async ({ todoId, isChecked }) => {
+  await write(last =>
+    last.map(todo => (todo.id === todoId ? { ...todo, isChecked } : todo)),
+  );
+};
